82f2c019740a6ab0c88d70da1f80a04f23a19674
Eduard Bondarenko <edbond@gmail.com>
2010-10-21 16:16:21 +0300

    jvm flags added

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
                      [lein-search "0.3.3"]
-                     [criterium "0.0.1-SNAPSHOT"]])
+                     [criterium "0.0.1-SNAPSHOT"]]
+  :jvm-flags ["-XX:+UseConcMarkSweepGC" "-Xmx1g" "-XX:+UseCompressedOops"])
